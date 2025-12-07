82e71237beb9fddf6a13d5a8d0e3e008204ef7df
Anica Rahman <anicarahman2009@yahoo.it>
2025-12-07 20:59:52 +0000

    contact server removed

--- src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendContactMessage } from "../controllers/ContactsController.js";
-
-const router = express.Router();
-
-router.post("/", sendContactMessage);
-
-export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bookingsRouter from "./routes/bookings.js";
-import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
@@ -11,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/bookings", bookingsRouter);
-app.use("/api/contact", contactRoutes);
 
 app.get("/health", (req, res) => res.json({ ok: true }));
 
