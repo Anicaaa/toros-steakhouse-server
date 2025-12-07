c9b2b8930abe1d03efc391d35a97300eb3dee58c
Anica Rahman <anicarahman2009@yahoo.it>
2025-12-07 22:28:19 +0000

    node env added

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // works on Render and locally
+  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
